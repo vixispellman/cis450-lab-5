3fddfb68d95956898ac7899d55001bdb5e59f556
vixi <vixi.mmv@proton.me>
2025-05-08 23:05:33 -0500

    Multiply: Use iN instead of i*N everywhere

--- multiplyOPT.c
+++ multiplyOPT.c
@@ -41,19 +41,19 @@ void transpose(long *A, long *B, int N) {
 
 void multiply_matrix_opt(long *A, long *B, long *C, int N)
 {
-  int i, j, k;
+  int iN, j, k;
   register long *B2;
   B2 = (long*)malloc(sizeof(long)*N*N);
   transpose(B,B2,N);
 
-  for (i=0; i<N; i++)
+  for (iN=0; iN<N*N; iN+=N)
     for (j=0; j<N; j++)
-      C[i*N+j]=0;
+      C[iN+j]=0;
 
-  for (i=0; i<N; i++)
+  for (iN=0; iN<N*N; iN+=N)
     for (j=0; j<N; j++)
       for (k=0; k<N; k++)
-        C[i*N+j] += A[i*N+k]*B2[j*N+k];
+        C[iN+j] += A[iN+k]*B2[j*N+k];
 
   free(B2);
 
